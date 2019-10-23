796f9d51c3a5050d9e0d2c57b6a2d3bf7fe56eab
Alon Tron <alontron@cs.huji.ac.il>
2019-10-22 21:04:01 -0700

    small minor fixes

--- neuralnet_starter.py
+++ neuralnet_starter.py
@@ -5,8 +5,8 @@ import pickle
 
 
 config = {}
-# config['layer_specs'] = [784, 100, 100, 10]  # The length of list denotes number of hidden layers; each element denotes number of neurons in that layer; first element is the size of input layer, last element is the size of output layer.
-config['layer_specs'] = [784] + [100] * 50 + [10]  # The length of list denotes number of hidden layers; each element denotes number of neurons in that layer; first element is the size of input layer, last element is the size of output layer.
+config['layer_specs'] = [784, 100, 100, 10]  # The length of list denotes number of hidden layers; each element denotes number of neurons in that layer; first element is the size of input layer, last element is the size of output layer.
+# config['layer_specs'] = [784] + [100] * 50 + [10]  # The length of list denotes number of hidden layers; each element denotes number of neurons in that layer; first element is the size of input layer, last element is the size of output layer.
 config['activation'] = 'sigmoid' # Takes values 'sigmoid', 'tanh' or 'ReLU'; denotes activation function for hidden layers
 config['batch_size'] = 1000  # Number of training samples per batch to be passed to network
 config['epochs'] = 50  # Number of epochs to train the model
@@ -111,96 +111,100 @@ class Layer():
     """
     Write the code for forward pass through a layer. Do not apply activation function here.
     """
-    self.a = np.matmul(np.c_[self.b,self.w],x)  # Weighted sum of x with weight matrix(augmented with bias)
+    x_with_one_rows = np.hstack((np.ones((x.shape[0],1)),x))
+    self.a = np.vstack((self.b,self.w)).T @ x_with_one_rows.T  # Weighted sum of x with weight matrix(augmented with bias)
     self.x = x
-    return self.a
+    return self.a.T
   
   def backward_pass(self, delta):
     """
     Write the code for backward pass. This takes in gradient from its next layer as input,
     computes gradient for its weights and the delta to pass to its previous layers.
     """
-    self.d_x = np.matmul(delta,np.transpose(np.c_[self.b,self.w]))
-    grad = np.matmul(delta,np.transpose(self.x))
-    self.d_b,self.d_w = np.split(grad,[1])
+    self.d_x = np.matmul(delta, np.vstack((self.b, self.w)).T)
+    grad = delta.T @ self.x
+    self.d_b,self.d_w = np.split(grad.T,[1])
     return self.d_x
 
       
 class Neuralnetwork():
-  def __init__(self, config):
-    self.layers = []
-    self.x = None  # Save the input to forward_pass in this
-    self.y = None  # Save the output vector of model in this
-    self.targets = None  # Save the targets in forward_pass in this variable
-    for i in range(len(config['layer_specs']) - 1):
-      self.layers.append( Layer(config['layer_specs'][i], config['layer_specs'][i+1]) )
-      if i < len(config['layer_specs']) - 2:
-        self.layers.append(Activation(config['activation']))  
-    
-  def forward_pass(self, x, targets=None):
-    """
-    Write the code for forward pass through all layers of the model and return loss and predictions.
-    If targets == None, loss should be None. If not, then return the loss computed.
-    """
-    self.x = x
-    self.targets = targets
+    def __init__(self, config):
+        self.layers = []
+        self.x = None  # Save the input to forward_pass in this
+        self.y = None  # Save the output vector of model in this
+        self.targets = None  # Save the targets in forward_pass in this variable
+        for i in range(len(config['layer_specs']) - 1):
+            self.layers.append( Layer(config['layer_specs'][i], config['layer_specs'][i+1]) )
+            if i < len(config['layer_specs']) - 2:
+                self.layers.append(Activation(config['activation']))
 
-    processed_x = x
-    for layer in self.layers:
-      processed_x = layer.forward_pass(processed_x)
+    def forward_pass(self, x, targets=None):
+        """
+        Write the code for forward pass through all layers of the model and return loss and predictions.
+        If targets == None, loss should be None. If not, then return the loss computed.
+        """
+        self.x = x
+        self.targets = targets
 
-    self.y = softmax(processed_x)
-    if targets != None:
-      loss = self.loss_func(self.y, self.targets)
+        processed_x = x
+        for layer in self.layers:
+            processed_x = layer.forward_pass(processed_x)
 
-    return loss, self.y
+        self.y = softmax(processed_x)
+        if targets is not None:
+            loss = self.loss_func(self.y, self.targets)
 
-  def loss_func(self, logits, targets):
-    amount_of_data = targets.shape[0]
-    return -np.sum(targets * np.log(logits)) / amount_of_data
+        return loss, self.y
 
-  def backward_pass(self):
-    # the gradient of cross-entropy on top of softmax is (t-y)
-    back_output = self.targets - self.y
-    weights_gradients = []
+    def loss_func(self, logits, targets):
+        amount_of_data = targets.shape[0]
+        return -np.sum(targets * np.log(logits)) / amount_of_data
 
-    for layer in reversed(self.layers):
-      back_output = layer.backward_pass(back_output)
-      if isinstance(layer, Layer):
-        weights_gradients.append(layer.d_w)
+    def backward_pass(self):
+        # the gradient of cross-entropy on top of softmax is (t-y)
+        back_output = self.targets - self.y
+        weights_gradients = []
 
-    # update weights with learning rule
-    for layer in self.layers:
-      if isinstance(layer, Layer):
-        alpha = config['learning_rate']
-        layer.w = layer.w - alpha * layer.d_w
-        layer.b = layer.b - alpha * layer.d_b
+        for layer in reversed(self.layers):
+            back_output = layer.backward_pass(back_output)
+            if isinstance(layer, Layer):
+                weights_gradients.append(layer.d_w)
 
-    loss = self.loss_func(self.y, self.targets)
-    return loss, self.y
+        loss = self.loss_func(self.y, self.targets)
+        return loss, self.y
 
-  def get_layers_weights_and_biases(self):
-      weights = []
-      biases = []
+    def update_weights_by_learning_rule(self):
+        for layer in self.layers:
+            if isinstance(layer, Layer):
+                alpha = config['learning_rate']
+                # TODO: add momentum and regularization
+                layer.w = layer.w - alpha * layer.d_w
+                layer.b = layer.b - alpha * layer.d_b
 
-      for layer in self.layers:
-          if isinstance(layer, Layer):
-              weights.append(layer.w)
-              biases.append(layer.b)
 
-      return np.array(weights), np.array(biases)
 
-  def apply_weights_and_biases_on_layers(self, weights, biases):
-      layer_index = 0
-      for layer in self.layers:
-          if isinstance(layer, Layer):
-              layer.w = weights[layer_index]
-              layer.b = biases[layer_index]
-              layer_index += 1
+    def get_layers_weights_and_biases(self):
+        weights = []
+        biases = []
 
+        for layer in self.layers:
+            if isinstance(layer, Layer):
+                weights.append(layer.w)
+                biases.append(layer.b)
+
+        return weights, biases
+
+    def apply_weights_and_biases_on_layers(self, weights, biases):
+        layer_index = 0
+        for layer in self.layers:
+            if isinstance(layer, Layer):
+                layer.w = weights[layer_index]
+                layer.b = biases[layer_index]
+                layer_index += 1
 
 def trainer(model, X_train, y_train, X_valid, y_valid, config):
-    best_weights, training_errors, validation_errors, training_accuracies, validation_accuracies\
+
+    training_errors, validation_errors, training_accuracies, validation_accuracies\
         = batch_stochastic_gradient_decent(model, X_train, y_train, X_valid, y_valid)
 
     plot_train_and_validation_loss(training_errors, validation_errors)
@@ -217,6 +221,8 @@ def plot_train_and_validation_loss(training_errors, validation_errors):
     plt.show()
 
 
+# batch and stochastic gradient decent implementation for two classes
+
 def plot_train_and_validation_accurecy(training_accurecies, validation_accurecies):
     plt.plot(training_accurecies, label="training accurecy")
     plt.plot(validation_accurecies, label="validation accurecy")
@@ -227,7 +233,6 @@ def plot_train_and_validation_accurecy(training_accurecies, validation_accurecie
     plt.show()
 
 
-# batch and stochastic gradient decent implementation for two classes
 def batch_stochastic_gradient_decent(nn, samples, labels, validation_samples, validation_labels):
     training_errors = []
     validation_errors = []
@@ -237,26 +242,32 @@ def batch_stochastic_gradient_decent(nn, samples, labels, validation_samples, va
     best_validation_loss = float('inf')
     current_weights, current_biases = nn.get_layers_weights_and_biases()
     best_weights = current_weights
+    best_biases = current_biases
 
     number_of_training_samples = samples.shape[0]
-    number_of_batch_in_epoch = number_of_training_samples / config['epochs']
+    number_of_batch_in_epoch = int(np.ceil(number_of_training_samples / config['epochs']))
 
     for t in range(config['epochs']):
         epoch_train_accuracies = []
         epoch_train_errors = []
 
         for i in range(number_of_batch_in_epoch):
-            batch_indices = np.random.shuffle(np.arange(number_of_training_samples))
+            batch_indices = np.arange(number_of_training_samples)
+            np.random.shuffle(batch_indices)
             batch_samples = samples[batch_indices]
             batch_labels = labels[batch_indices]
 
             batch_loss, predictions = nn.forward_pass(batch_samples, batch_labels)
+
             # need to calculate the train loss and accuracy for each batch and then average it to ger the epochs
             update_single_batch_loss_and_accuracy(batch_loss, predictions, batch_labels, epoch_train_accuracies, epoch_train_errors)
 
             nn.backward_pass()
+            nn.update_weights_by_learning_rule()
+
             current_weights, current_biases = nn.get_layers_weights_and_biases()
 
+        # TODO: stop if error going up for X continuous times
         update_train_loss_and_acuuracy(epoch_train_accuracies, epoch_train_errors, training_accuracies, training_errors)
         current_validation_loss = update_validation_loss_and_accuracy(nn, validation_accuracies,
                                                                       validation_errors, validation_labels,
@@ -265,9 +276,10 @@ def batch_stochastic_gradient_decent(nn, samples, labels, validation_samples, va
         if best_validation_loss > current_validation_loss:
             best_validation_loss = current_validation_loss
             best_weights = current_weights
+            best_biases = current_biases
 
-    return best_weights, np.array(training_errors), np.array(validation_errors),\
-                         np.array(training_accuracies), np.array(validation_accuracies)
+    nn.apply_weights_and_biases_on_layers(best_weights, best_biases)
+    return np.array(training_errors), np.array(validation_errors), np.array(training_accuracies), np.array(validation_accuracies)
 
 
 def update_single_batch_loss_and_accuracy(batch_loss, predictions, batch_labels, epoch_train_accuracies, epoch_train_errors):
@@ -311,21 +323,15 @@ def test(model, X_test, y_test, config):
 
 
 
-# calculate the specific weights accuracy
-def get_softmax_weights_accuracy(pca_images, labels, weights):
-
-      
-
 if __name__ == "__main__":
-  train_data_fname = 'MNIST_train.pkl'
-  valid_data_fname = 'MNIST_valid.pkl'
-  test_data_fname = 'MNIST_test.pkl'
-  
-  ### Train the network ###
-  model = Neuralnetwork(config)
-  X_train, y_train = load_data(train_data_fname)
-  X_valid, y_valid = load_data(valid_data_fname)
-  X_test, y_test = load_data(test_data_fname)
-  trainer(model, X_train, y_train, X_valid, y_valid, config)
-  test_acc = test(model, X_test, y_test, config)
+    train_data_fname = 'MNIST_train.pkl'
+    valid_data_fname = 'MNIST_valid.pkl'
+    test_data_fname = 'MNIST_test.pkl'
 
+    ### Train the network ###
+    model = Neuralnetwork(config)
+    X_train, y_train = load_data(train_data_fname)
+    X_valid, y_valid = load_data(valid_data_fname)
+    X_test, y_test = load_data(test_data_fname)
+    trainer(model, X_train, y_train, X_valid, y_valid, config)
+    test_acc = test(model, X_test, y_test, config)
